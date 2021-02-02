5323f7ed4ad14c34139a8747e92835c995d30853
webtimise <hello@webtimise.fr>
2021-02-02 21:41:35 +0100

    test

--- morpion.js
+++ morpion.js
@@ -8,3 +8,18 @@ function initialisation() {
         contenu = case_cliquee.textContent;
     }
 }
+
+document.addEventListener("DOMContentLoaded", function() 
+{ 
+	console.log("Le document est pret");
+	var joueur="X";
+	document.getElementById('morpion').addEventListener("mousedown", function(e) 
+	{
+		console.log("Le bouton de la souris a été enfoncé.");
+		var td=e.target;
+		if(td.textContent!==''){return;}
+		td.textContent=joueur;
+		joueur=(joueur==="X" ? 'O' : 'X');
+	});
+
+});
